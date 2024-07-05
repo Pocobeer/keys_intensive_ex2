d8a3d228b298a7254a515bdf030478ddc31831a3
Pocobeer <vall.com3454.71@gmail.com>
2024-07-05 11:36:16 +0300

    ds

---
+++ dishes/cook/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Povar(models.Model):
+    Povar_name = models.CharField(max_length=50)
+    Povar_experience = models.IntegerField()
+    Povar_rating = models.FloatField()
+
+
+
+class Ingredients(models.Model):
+    ingredient_name = models.CharField(max_length=50)
+    ingredient_price = models.FloatField()
+    types = (
+        (0, "fish"),
+        (1, "meat"),
+        (2, "vegetables"),
+    )
+    ingredient_type = models.IntegerField(choices=types, default=0)
+
+class Dish(models.Model):
+    dish_name = models.CharField(max_length=50)
+    dish_price = models.FloatField()
+    dish_rating = models.FloatField()
+    dish_ingredients = models.ManyToManyField(Ingredients)
+    dish_povars = models.ManyToManyField(Povar)
