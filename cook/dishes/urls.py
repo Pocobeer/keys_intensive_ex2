47546b31f5e8e1ac2876342a4e6f167a07e9c057
Pocobeer <vall.com3454.71@gmail.com>
2024-07-11 22:02:14 +0300

    s

--- cook/dishes/urls.py
+++ cook/dishes/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import PovarViewSet, IngredientsListCreate, DishListCreate, DishDetail
 
-router = DefaultRouter()
-router.register(r'povars', PovarViewSet)
+from . import views
 
 urlpatterns = [
-    path('ingredients/', IngredientsListCreate.as_view(), name='ingredients-list-create'),
-    path('dishes/', DishListCreate.as_view(), name='dishes-list-create'),
-    path('dishes/<int:pk>/', DishDetail.as_view(), name='dish-detail'),
+    path('', views.index, name='index'),
 ]
-
-urlpatterns += router.urls
